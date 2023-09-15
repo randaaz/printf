c0e8eba23d6c053b15f3b14fae92c482bb9133e8
randaaz <randaaziz972@gmail.com>
2023-09-15 13:16:53 -0700

    modifying the print_d.c file by adding convert_to_binary function and modify some errors

--- print_d.c
+++ print_d.c
@@ -2,7 +2,7 @@
 
 /**
  * print_intger - print intger numbers
- * @args: The va_list containing the character to print
+ * @args: The va_list containing the intger to print
  */
 
 void print_intger(va_list args)
@@ -11,6 +11,12 @@ void print_intger(va_list args)
 	char size[20];
 	int i = 0;
 
+	if (number == 0)
+	{
+		_putchar('0');
+		return;
+
+	}
 	if (number < 0)
 	{
 		_putchar('-');
@@ -26,5 +32,35 @@ void print_intger(va_list args)
 	while (i > 0)
 	{
 		_putchar(size[--i]);
+	}
+}
+
+
+/**
+ * converted_to_binary - Converts an unsigned int
+ * number to binary and prints it.
+ * @args: The va_list containing the intger to  converted
+ */
+void converted_to_binary(va_list args)
+{
+	int j, i = 0, a[32];
+	unsigned int n = va_arg(args, unsigned int);
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (n > 0)
+	{
+		a[i] = n % 2;
+		i++;
+		n /= 2;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(a[j] + '0');
 	}
 }
