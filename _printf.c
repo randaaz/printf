907d0c8dd842dbb0704de9011b929cfa3dc8b28a
randaaz <randaaziz972@gmail.com>
2023-09-15 10:13:41 -0700

    add the _printf.c file

---
+++ _printf.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _printf - Custom printf function.
+ * @format: The format string.
+ * @...: Variable arguments.
+ *
+ * Return: The number of characters printed (excluding the null byte).
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int j = 0;
+
+	va_start(args, format);
+
+	for (int i = 0; format && format[i]; i++)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			j++;
+		}
+		else if (format[i + 1])
+		{
+			i++;
+			j += _format_specifier(format[i], args);
+		}
+	}
+
+	va_end(args);
+	return (j);
+}
