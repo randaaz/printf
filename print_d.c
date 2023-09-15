db1c02618076e0060ffdff32610499d956d1c659
rashaaz <rashaabdelaziz111@gmail.com>
2023-09-15 09:50:38 -0700

    add print_d.c

---
+++ print_d.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_intge - print intger numbers
+ * @args: The va_list containing the character to print
+ */
+
+void print_intger(va_list args)
+{
+	int number = va_arg(args, int);
+	char size[20];
+	int i = 0;
+
+	if (number < 0)
+	{
+		_putchar('-');
+		number = -number;
+	}
+
+	while (number > 0)
+	{
+		size[i++] = number % 10 + '0';
+		number /= 10;
+	}
+
+	while (i > 0)
+	{
+		_putchar(size[--i]);
+	}
+}
