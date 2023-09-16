62aa3ae1e81abd79049a760325ce4520a0becf1f
randaaz <randaaziz972@gmail.com>
2023-09-16 04:51:15 -0700

    make it with betty style

--- _printf.c
+++ _printf.c
@@ -10,40 +10,41 @@
 
 int _printf(const char *format, ...)
 {
-    int characters_printed = 0;
-    va_list args;
+	int characters_printed = 0;
+	int specifier_chars_printed;
+	va_list args;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    if (!format)
-        return (-1);
+	if (!format)
+		return (-1);
 
-    while (*format)
-    {
-        if (*format != '%')
-        {
-            _putchar(*format);
-            characters_printed++;
-        }
-        else
-        {
-            format++;
-            if (*format)
-            {
-                int specifier_chars_printed = _format_specifier(*format, args);
-                if (specifier_chars_printed < 0)
-                    return (-1);
-                characters_printed += specifier_chars_printed;
-            }
-            else
-            {
-                va_end(args);
-                return (-1);
-            }
-        }
-        format++;
-    }
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			_putchar(*format);
+			characters_printed++;
+		}
+		else
+		{
+			format++;
+			if (*format)
+			{
+				specifier_chars_printed = _format_specifier(*format, args);
+				if (specifier_chars_printed < 0)
+					return (-1);
+				characters_printed += specifier_chars_printed;
+			}
+			else
+			{
+				va_end(args);
+				return (-1);
+			}
+		}
+		format++;
+	}
 
-    va_end(args);
-    return (characters_printed);
+	va_end(args);
+	return (characters_printed);
 }
