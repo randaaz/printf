831e0333fd56476a2a5f8b1f09b02da3e56502cb
randaaz <randaaziz972@gmail.com>
2023-09-16 08:43:19 -0700

    modify print_many_characters.c

--- print_many_characters.c
+++ print_many_characters.c
@@ -9,37 +9,5 @@
  */
 int _putchar(char c)
 {
-	static int i;
-	static char buffer[BUFFER_s];
-
-	if (c == BUFFER_D || i >= BUFFER_s)
-	{
-		write(1, buffer, i);
-		i = 0;
-	}
-	if (c != BUFFER_D)
-	{
-		buffer[i++] = c;
-	}
-	return (1);
-}
-
-/**
- * print_many_characters - Prints many characters followed by a new line.
- * @ptr: Pointer to characters.
- *
- * Return: The number of characters printed.
- */
-
-int print_many_characters(const char *ptr)
-{
-	int count = 0, i;
-
-	for (i = 0; ptr[i] != '\0'; i++)
-	{
-		_putchar(ptr[i]);
-		count++;
-	}
-
-	return (count);
+	return (write(1, &c, 1));
 }
