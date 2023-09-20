c725a2531f646fd7e2f1dff52cec19bfcf4a359c
randaaz <randaaziz972@gmail.com>
2023-09-19 18:47:01 -0700

    modify

--- file1.c
+++ file1.c
@@ -44,7 +44,7 @@ char *Convert_integers_to_a_string(long int numb,
 
 
 /**
- * print_n - Format a string with specified width and flags.
+ * print_n - Format a string with specified width and flags
  * @ptr: The string to format.
  * @pp: Pointer to a pp_p structure
  *
